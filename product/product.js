b877de9fa0e54c914aef90e28fdb4799a78a5613
[SW5] 문수민 <sum95771@gmail.com>
2023-07-06 02:32:01 +0900

    Design : 상품페이지 CSS 수정

--- product/product.js
+++ product/product.js
@@ -1,4 +1,5 @@
 const itemList = document.querySelector(".products-items");
+
 for (let i = 1; i <= 16; i++) {
   const item = document.createElement("div");
 
@@ -6,20 +7,20 @@ for (let i = 1; i <= 16; i++) {
 
   item.innerHTML = `
     <div class="item-row">
-                <img id="itemId" src="../img/men-top-${i}.jpg" alt="" />
-                <i class="fa-regular fa-star"></i>
+                <img id="itemId" src="../public/asset/img/men-top-${i}.jpg" alt="" />
+                <i class="fa-solid fa-star"></i>
               </div>
               <div class="item-row">
                 <div class="item-brand">브랜드명</div>
                 <div class="item-name">상품명</div>
                 <div class="item-price">00000원</div>
                 <div class="item-review">
-                  <i class="fa-regular fa-star"></i>
-                  <span class="item-rating">5.0/5</span>
+                  <i class="fa-solid fa-star"></i>
+                  <span class="item-rating"><strong>5.0</strong>/5</span>
                   <span class="item-bar"></span>
                   <span class="item-review-number">리뷰 1</span>
                 </div>
-                <div class="item-new">신상품</div>
+                
               </div>
   `;
   itemList.appendChild(item);
