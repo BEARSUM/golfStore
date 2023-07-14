0045a4285c7ac0dc3b5d073c2c0ff50e6c4f3cff
[SW5] 한규정 <rbwjdrbxo32@gmail.com>
2023-07-14 19:31:14 +0900

    test

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,31 +1,41 @@
-import { API_URL, getAPI } from './common.js';
-getmembers();
-
-async function getmembers() {
+// JavaScript 파일 (member.js)
+document.addEventListener("DOMContentLoaded", async function () {
   try {
-    const token = localStorage.getItem('token');
-    const data = await getAPI(`${API_URL}/users/token/${token}`, {
-      method: 'GET',
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMTFAYWRtaW4uY29tIiwiaWF0IjoxNjg5MzMwNDE4LCJleHAiOjE2ODk0MTY4MTh9.rR-quAzqpC9ihN8kejwNCsjMVPlQ0D4L-azdrsyqXDM";
+
+    const response = await fetch(
+      "http://kdt-sw-5-team06.elicecoding.com/users",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("회원 정보를 가져오는데 실패했습니다.");
+    }
+
+    const users = await response.json();
+
+    const userTable = document.querySelector(".userlist");
+    userTable.innerHTML = "";
+
+    users.forEach((user) => {
+      const tableRow = document.createElement("tr");
+      tableRow.innerHTML = `
+        <td>${user.email}</td>
+        <td>${user.name}</td>
+        <td>${user.phoneNumber}</td>
+        <td>
+          <a href="manageMemberDetail.html?id=${user._id}">자세히 보기</a>
+        </td>
+      `;
+
+      userTable.appendChild(tableRow);
     });
-
-    membersList(data.users);
-  } catch (e) {
-    alert(e);
+  } catch (error) {
+    console.error(error);
   }
-}
-
-function membersList(users) {
-  const userList = document.querySelector('#userList');
-  userList.innerHTML = '';
-  for (let i = 0; i < users.length; i++) {
-    const item = document.createElement('tr');
-    const { email, name, phoneNumber, birthdate } = users[i];
-    item.innerHTML = `<tr class="user-list">
-      <td>${birthdate}</td>
-      <td>${email}</td>
-      <td>${name}</td>
-      <td>${phoneNumber}</td>
-    </tr>`;
-    userList.appendChild(item);
-  }
-}
+});
--- assets/js/productAdd.js
+++ assets/js/productAdd.js
@@ -1,23 +1,23 @@
-import { API_URL, getAPI } from './common.js';
+// import { API_URL, getAPI } from './common.js';
 
-const form = document.getElementById('product-form');
+// const form = document.getElementById('product-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(form);
-  addProduct(formData);
-});
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const formData = new FormData(form);
+//   addProduct(formData);
+// });
 
-async function addProduct(formData) {
-  for (const [key, value] of formData) {
-    console.log(key, value);
-  }
-  try {
-    const data = await getAPI(`${API_URL}/products`, {
-      method: 'POST',
-      body: formData,
-    });
-  } catch (e) {
-    alert(e);
-  }
-}
+// async function addProduct(formData) {
+//   for (const [key, value] of formData) {
+//     console.log(key, value);
+//   }
+//   try {
+//     const data = await getAPI(`${API_URL}/products`, {
+//       method: 'POST',
+//       body: formData,
+//     });
+//   } catch (e) {
+//     alert(e);
+//   }
+// }
